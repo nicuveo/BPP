4de21b2bb03347986186ca532a3cab2460c40f7a
Nicuvëo <crucuny@gmail.com>
2019-01-11 18:20:50 +0000

    Cleaned location Show instance.

--- src/Module.hs
+++ src/Module.hs
@@ -22,10 +22,12 @@ data WithLocation a = WL { getLocation :: Location
                          , getEntry    :: a
                          }
 
+instance Show Location where
+  show BuiltIn = "<built-in function>"
+  show (SourceFile s l c) = printf "%s:%d:%d" s l c
+
 instance Show a => Show (WithLocation a) where
-  show (WL loc x) = case loc of
-    BuiltIn          -> printf "<built-in function>: %s" $ show x
-    SourceFile s l c -> printf "%s:%d:%d: %s" s l c      $ show x
+  show (WL loc x) = printf "%s: %s" (show loc) $ show x
 
 instance Functor WithLocation where
   fmap f (WL l x) = WL l $ f x
