0193ff9949df1ba3ef5ec49f797a53d1596ccc5f
Nicuvëo <crucuny@gmail.com>
2019-01-15 22:47:19 +0000

    Added simple but functional main.

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,8 @@ module Main where
 
 import           Data.List
 import           System.Directory
-
+import           System.Environment
+import           System.Exit
 
 import           Assembler
 import           BuiltIn
@@ -24,48 +25,24 @@ fileResolver filename  = do
     else Just . (rfn,) <$> readFile rfn
 
 
+help :: IO a
+help = putStrLn "usage: bfs [-O] file.bs" >> exitFailure
+
+options :: IO (Bool, String)
+options = do
+  args <- getArgs
+  case args of
+    ["-O", f] -> return (True, f)
+    [      f] -> return (False, f)
+    _         -> help
 
 main :: IO ()
 main = do
-  (diags, mObjs) <- compile testResolver "interactive"
+  (dense, source) <- options
+  (diags, mObjs)  <- compile fileResolver source
   mapM_ print diags
   case mObjs of
     Nothing -> putStrLn "Aborting."
     Just o  -> do
-      putStr =<< either error return (assembleVerbosely o)
-      putStr =<< either error return (assembleDensely o)
-
-
-
-testResolver :: String -> IO (Maybe (String, String))
-testResolver "interactive" = return $ Just ("interactive", testData)
-testResolver "other"       = return $ Just ("other.hs", otherData)
-testResolver "Prelude"     = Just . ("Prelude.bs",) <$> preludeFile
-testResolver _             = error "NIH"
-
-oldTestData :: String
-oldTestData = "// Test\
-              \\ninclude \"other\"\
-              \\n\
-              \\nconst I s = 3\
-              \\nconst C s2 = 0x42\
-              \\n\
-              \\ndef IMPURE foo() [] -> [] { ++--++ BAR }\n\
-              \\ndef foo2(I x, C y) [I,I] -> [I] { BAZ(42, x) }\
-              \\n"
-
-otherData :: String
-otherData = "def IMPURE BAR() [C] -> [] {}"
-
-testData :: String
-testData = "// Test\
-           \\nconst I s1 = 3\
-           \\nconst C s2 = 0x42\
-           \\nconst C s3 = 'x'\
-           \\nconst S s4 = \"test\"\
-           \\n\
-           \\ndef impure inline true() [] -> [B] { >+ }\n\
-           \\ndef impure inline foo() [] -> [] { ++--++ }\n\
-           \\ndef impure baz(I a, C b, S c) [] -> [] { foo [-] foo() }\
-           \\ndef main() { foo baz(s3, 42, \"test\") foo pushc(220) if(true) { set(200) } prints(\"toto\\n\") popc }\
-           \\n"
+      let assembler = if dense then assembleDensely else assembleVerbosely
+      putStr =<< either error return (assembler o)
